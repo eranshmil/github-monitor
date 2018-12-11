ce2356e27c9c76c7fa0e4cbcd75980e74806b679
eranshmil <eranshmil@users.noreply.github.com>
2018-12-11 17:26:25 +0200

    fix(client): make sidenav responsive

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sml-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private _translate: TranslateService) {}
+export class AppComponent implements OnInit, OnDestroy {
+  public isMobile: boolean;
+
+  private _mobileBreakpointSubscription: Subscription;
+
+  constructor(
+    private _translate: TranslateService,
+    private _breakpointObserver: BreakpointObserver
+  ) {}
+
+  /**
+   * Control sidenav behaviors in mobile/desktop.
+   */
+  private _initResponsiveSidenav() {
+    this._mobileBreakpointSubscription = this._breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe(breakpointState => (this.isMobile = breakpointState.matches));
+  }
 
   ngOnInit() {
     this._translate.use('en');
+
+    this._initResponsiveSidenav();
+  }
+
+  ngOnDestroy() {
+    this._mobileBreakpointSubscription.unsubscribe();
   }
 }
